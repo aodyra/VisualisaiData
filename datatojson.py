7022db72ca3e528559ddaecc976516546cff58e3
aodyra <aodyra@gmail.com>
2017-05-16 18:37:47 +0700

    push dashboard

--- datatojson.py
+++ datatojson.py
@@ -27,6 +27,8 @@ tournament_list['ATP250'] = []
 tournament_list['ATP500'] = []
 tournament_list['Grand Slam'] = []
 tournament_list['Masters 1000'] = []
+tournament_list['ATP World Tour Finals'] = []
+tournament_list['Summer Olympic'] = []
 
 for rownum in range(1, sh.nrows):
     tournament = {}
@@ -37,15 +39,23 @@ for rownum in range(1, sh.nrows):
     tournament['date'] = cell_as_datetime.day
     tournament['month'] = string_month(cell_as_datetime.month)
     tournament['series'] = row_values[3]
-    if tournament['series'] == 'Masters Cup': continue
     tournament['court'] = row_values[4]
     tournament['surface'] = row_values[5]
     tournament['best_of'] = row_values[6]
-    tournament['winner'] = row_values[7]
-    tournament['location'] = row_values[8]
-    tournament['country'] = row_values[9]
-    tournament['latitude'] = row_values[10]
-    tournament['longitude'] = row_values[11]
+    tournament['single_winner'] = row_values[7]
+    tournament['single_winner_nasionalities'] = row_values[8]
+    tournament['double_winner_1'] = row_values[9]
+    tournament['double_winner_1_nasionalities'] = row_values[10]
+    tournament['double_winner_2'] = row_values[11]
+    tournament['double_winner_2_nasionalities'] = row_values[12]
+    tournament['mixed_double_double_winner_1'] = row_values[13]
+    tournament['mixed_double_double_winner_1_nasionalities'] = row_values[14]
+    tournament['mixed_double_double_winner_2'] = row_values[15]
+    tournament['mixed_double_double_winner_2_nasionalities'] = row_values[16]
+    tournament['location'] = row_values[17]
+    tournament['country'] = row_values[18]
+    tournament['latitude'] = row_values[19]
+    tournament['longitude'] = row_values[20]
     if tournament['series'] == 'ATP250':
         tournament_list['ATP250'].append(tournament)
     elif tournament['series'] == 'ATP500':
@@ -54,7 +64,11 @@ for rownum in range(1, sh.nrows):
         tournament_list['Grand Slam'].append(tournament)
     elif tournament['series'] == 'Masters 1000':
         tournament_list['Masters 1000'].append(tournament)
+    elif tournament['series'] == 'ATP World Tour Finals':
+        tournament_list['ATP World Tour Finals'].append(tournament)
+    elif tournament['series'] == 'Summer Olympic':
+        tournament_list['Summer Olympic'].append(tournament)
 
 j = json.dumps(tournament_list)
-with open('data.json', 'w') as f:
+with open('data2.json', 'w') as f:
     f.write(j)
